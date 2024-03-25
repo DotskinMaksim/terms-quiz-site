37850397e0e6b868dd82ebfa1da8ba7342cbe602
opilane <maxell9976@gmail.com>
2024-03-25 15:40:47 +0200

    Lisasin komentaarid[#187304319]

--- tarkvaraSonavara/script.js
+++ tarkvaraSonavara/script.js
@@ -69,14 +69,14 @@ const sonadVeneKeeles=[
 
 
 function randomSona(){
-    // metood võta juhuslik sõna
+    // metood võta juhuslik sõna eesti keelse masiivist
     const randId = Math.floor(Math.random() * sonadEestiKeeles.length);
     const sona = sonadEestiKeeles[randId];
     document.getElementById('randomSonad').innerHTML = sona;
     document.getElementById('hiddenInput').value = randId;
 }
 function randomSona2(){
-    // metood võta juhuslik sõna
+    // metood võta juhuslik sõna aga vene keelse masiviist
     const randId = Math.floor(Math.random() * sonadVeneKeeles.length);
     const sona = sonadVeneKeeles[randId];
     document.getElementById('randomSonad2').innerHTML = sona;
@@ -86,22 +86,27 @@ function randomSona2(){
 
 function kontroll(){
     //metood mis kontrollib vastus
-    const randId = document.getElementById('hiddenInput').value;
-    const vastus = document.getElementById('kontroll').value.toLowerCase();;
+    const randId = document.getElementById('hiddenInput').value; //võtame sõna masiivis id
+    const vastus = document.getElementById('kontroll').value.toLowerCase();; // võtame kasutaja vastust
+
+
+    // kui vastus on vene keelse masiivis
 
     if (vastus === sonadVeneKeeles[randId]){
-        document.getElementById('vastus').innerHTML = "<span style=\"color: green;\">Õige!</span>";
+        document.getElementById('vastus').innerHTML = "<span style=\"color: green;\">Õige!</span>";//siis koreektne
     } else {
-        document.getElementById('vastus').innerHTML = "<span style=\"color: red;\">Vale!</span>";
+        document.getElementById('vastus').innerHTML = "<span style=\"color: red;\">Vale!</span>";// kui vastupidi siis ebakoreektne
     }
 }
 function kontroll2(){
     //metood mis kontrollib vastus
-    const randId = document.getElementById('hiddenInput2').value;
-    const vastus = document.getElementById('kontroll2').value.toLowerCase();;
+    const randId = document.getElementById('hiddenInput2').value; //võtame sõna masiivis id
+    const vastus = document.getElementById('kontroll2').value.toLowerCase();;// võtame kasutaja vastust
 
+    // kui vastus on eesti keelse masiivis
     if (vastus === sonadEestiKeeles[randId]){
-        document.getElementById('vastus2').innerHTML = "<span style=\"color: green;\">Правильно!</span>";
+        document.getElementById('vastus2').innerHTML = "<span style=\"color: green;\">Правильно!</span>"; //siis koreektne
+        // kui vastupidi siis ebakoreektne
     } else {
         document.getElementById('vastus2').innerHTML = "<span style=\"color: red;\">Не правильно!</span>";
     }
